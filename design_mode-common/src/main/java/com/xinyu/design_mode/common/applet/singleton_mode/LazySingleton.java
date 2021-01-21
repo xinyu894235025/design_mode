65978357d4b761f0bfe10edd0838c0d237773558
xinyu4 <894235025@qq.com>
2021-01-21 11:05:55 +0800

    格式

--- design_mode-common/src/main/java/com/xinyu/design_mode/common/applet/singleton_mode/LazySingleton.java
+++ design_mode-common/src/main/java/com/xinyu/design_mode/common/applet/singleton_mode/LazySingleton.java
@@ -7,10 +7,10 @@ import java.util.Objects;
  * @Author xinyu4
  * @Date 2021/1/20/0020 17:40
  * @desc 懒加载单例模式（懒汉式）
- *  这种方式是最基本的实现方式，这种实现最大的问题就是不支持多线程。
- *  因为没有加锁 synchronized，所以严格意义上它并不算单例模式。
- *  这种方式 lazy loading 很明显，不要求线程安全，在多线程不能正常工作。
- *  线程安全只需要在方法体上加入 synchronized 关键字，但是会影响效率
+ * 这种方式是最基本的实现方式，这种实现最大的问题就是不支持多线程。
+ * 因为没有加锁 synchronized，所以严格意义上它并不算单例模式。
+ * 这种方式 lazy loading 很明显，不要求线程安全，在多线程不能正常工作。
+ * 线程安全只需要在方法体上加入 synchronized 关键字，但是会影响效率
  */
 public class LazySingleton {
 
